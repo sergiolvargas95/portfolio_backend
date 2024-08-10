4321b84c17e5dd6ad3696906f9cf54959c884922
sergiolvargas95 <sergiolvargas95@gmail.com>
2024-08-10 15:35:11 -0500

    feat(entities): create columns in user to implement security

--- src/main/java/com/example/backend/models/UserEntity.java
+++ src/main/java/com/example/backend/models/UserEntity.java
@@ -26,6 +26,18 @@ public class UserEntity {
     @Column(name = "password", nullable = false, length = 150)
     private String password;
 
+    @Column(name = "is_enabled")
+    private boolean isEnabled;
+
+    @Column(name = "account_No_Expired")
+    private boolean accountNoExpired;
+
+    @Column(name = "account_No_Locked")
+    private boolean accountNoLocked;
+
+    @Column(name = "credential_No_Expired")
+    private boolean credentialNoExpired;
+
     @OneToMany
     @JoinColumn(name = "user_id")
     private List<Profile> profileList;
